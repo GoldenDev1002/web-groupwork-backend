7cc772df2bdbc569a75901aa384d989ba1b4117e
GoldenDev1002 <jenzielol1002@gmail.com>
2023-01-08 23:49:32 +0000

    commit 5

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ let cors = require("cors");
 app.use(cors());
 app.get("/lessons", (req, res)=> {
     app.set("json spaces", 3);
+    let lessons= [
+        {
+        topic: "Math",
+            location: "Brent Cross", 
+            price: 100
+           
+        },
